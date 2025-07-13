500614d168dda6aa6bcf4b2fac88ea2142b40157
eslasojica <keslasojica@gmail.com>
2025-07-14 06:32:58 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Sma Bocil';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Jepang Viral Sma Cindo Tante China';
-export const url = 'https://bokepsmabocil.pages.dev';
-export const terbit = '2025-06-25T00:01:03Z';
